70195361792865feb9a3856031db03d30e2372fe
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-07-12 02:28:45 -0300

    Bugfix do link para cep

--- src/Search.php
+++ src/Search.php
@@ -2,15 +2,28 @@
 
 namespace Gabriel\DigitalCep;
 
-class Search {
-    private $url = "https://viacep.com.br/ws";
+class Search{
+    private $url = "https://viacep.com.br/ws/";
 
     public function getAddressFromZipcode(string $zipCode): array{
-        $zipCode = preg_replace('/[^8=9]/im', '', $zipCode);
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
 
-        $get = file_get_contents($this->url . $zipCode . "/json");
+        $get = $this->getFromServer($zipCode);
 
         return (array) json_decode($get);
     }
-}
 
+    private function getFromServer($zipCode){
+        $get = file_get_contents($this->url . $zipCode . "/json");
+
+        return $get;
+    }
+
+    private function processData($data){
+        foreach( $data as $k => $v ){
+            $data[$k] = trim($v);
+        }
+
+        return $data;
+    }
+}
